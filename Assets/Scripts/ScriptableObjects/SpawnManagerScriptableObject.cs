d751df1149c0e030672b3dad68d2e3a34395fd7a
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-05-12 14:14:32 -0500

    Custom Event System

---
+++ Assets/Scripts/ScriptableObjects/SpawnManagerScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SpawnManager", menuName = "ScriptableObjects/SpawnManager")]
+public class SpawnManagerScriptableObject : ScriptableObject
+{
+    public string prefabName;
+    public int spawnCount;
+    public float timeBetweenSpawns;
+}
